6cd4f3359a787250d99d0e08f89be6d551e4fdc9
Thomas Regourd <tom.reg@hotmail.fr>
2022-12-12 16:04:31 +0100

    [test] add basic navigation test

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import courseData from "./data/coursesData.json";
 
 describe("Navigate to My Course", () => {
   let browser;
@@ -13,11 +14,17 @@ describe("Navigate to My Course", () => {
   });
 
   it("should display all courses when clicking on `My Course`", async () => {
+    // Navigate to My Course
     await page.goto("http://localhost:3001");
     const [button] = await page.$x("//a[contains(., 'My Progress')]");
     await button.click();
     const text = await page.$eval("#MyCourse", (e) => e.textContent);
     expect(text).toBeTruthy();
+
+    // Verify that there is as many card as in courseData
+
+    const cards = await page.$$("#CourseCard");
+    expect(cards.length).toEqual(courseData.length);
   });
 
   afterAll((done) => {
--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -51,7 +51,7 @@ export default function Progress() {
         columns={{ xs: 4, sm: 8, md: 12 }}
       >
         {courses.map((course, index) => (
-          <Grid item xs={2} sm={4} md={4} key={index}>
+          <Grid item xs={2} sm={4} md={4} key={index} id="CourseCard">
             <CourseCard course={course} />
           </Grid>
         ))}
